ef9b6ca081a8c99e28dd421afc203657bb281dd5
liang <1399353869@qq.com>
2024-05-28 22:33:45 -0700

    message 5: The first iterative developmen

--- notes/lists/views.py
+++ notes/lists/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-def home_page():
+def home_page(requset):
+    return HttpResponse('<html><title>To-Do lists</title></html>')
     pass
 
 # Create your views here.
