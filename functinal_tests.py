46ffaf167e9164da532a2c75e59c26aa62b815fa
liang <1399353869@qq.com>
2024-05-29 01:24:56 -0700

    message 9: After refactoring the functional_tests

--- functinal_tests.py
+++ functinal_tests.py
@@ -8,10 +8,16 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
-
+    
     def tearDown(self):
         self.browser.quit()
+   
     
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID,'id_list_table')
+        rows = table.find_elements(By.TAG_NAME,'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
@@ -29,22 +35,22 @@ class NewVisitorTest(unittest.TestCase):
 
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element(By.ID,'id_list_table')
-        rows = table.find_elements(By.TAG_NAME,'tr')
-        self.assertIn('1: Buy flowers', [row.text for row in rows])
-
+        self.check_for_row_in_list_table('1: Buy flowers')
+        
         inputbox = self.browser.find_element(By.ID,'id_new_item')
         inputbox.send_keys('Give a gift to Lisi')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element(By.ID,'id_list_table')
-        rows = table.find_elements(By.TAG_NAME,'tr')
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
         self.assertIn('1: Buy flowers', [row.text for row in rows])
         self.assertIn('2: Give a gift to Lisi', [row.text for row in rows])
 
-
+        self.check_for_row_in_list_table('1: Buy flowers')
+        self.check_for_row_in_list_table('2: Give a gift to Lisi')
+        
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
