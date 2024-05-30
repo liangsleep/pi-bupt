1b842cd4b089e395b125108dba0998b3fbf47e90
liang <1399353869@qq.com>
2024-05-30 00:44:51 -0700

    message 16: form URL of new list by POST

--- notes/lists/tests.py
+++ notes/lists/tests.py
@@ -17,4 +17,14 @@ class ListViewTest(TestCase):
         response = self.client.get('/lists/the-new-page/')
         self.assertTemplateUsed(response, 'list.html')
 
-    
+class NewListTest(TestCase):
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/lists/new',data={'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new',data={'item_text': 'A new list item'})
+        self.assertRedirects(response, '/lists/the-new-page/')
