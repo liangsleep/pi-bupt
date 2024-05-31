a8795975184cb323fc93cd1bab58cf8c4d1da4a2
liang <1399353869@qq.com>
2024-05-31 02:00:29 -0700

    message 17: change models for multiple users

--- notes/lists/tests.py
+++ notes/lists/tests.py
@@ -3,7 +3,7 @@ from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-from lists.models import Item
+from lists.models import Item, List
 
 class HomePageTest(TestCase):
 
@@ -17,6 +17,11 @@ class ListViewTest(TestCase):
         response = self.client.get('/lists/the-new-page/')
         self.assertTemplateUsed(response, 'list.html')
 
+    def test_displays_all_list_items(self):
+        list_user = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_user)
+        Item.objects.create(text='itemey 2', list=list_user)
+
 class NewListTest(TestCase):
 
     def test_can_save_a_POST_request(self):
@@ -28,3 +33,33 @@ class NewListTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/lists/new',data={'item_text': 'A new list item'})
         self.assertRedirects(response, '/lists/the-new-page/')
+
+class ListAndItemModelsTest(TestCase):
+    
+    def test_saving_and_retrieving_items(self):
+
+        list_user = List()
+        list_user.save()
+
+        first_item = Item()
+        first_item.text = 'The first list item'
+        first_item.list = list_user
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.list = list_user
+        second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_user)
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first list item')
+        self.assertEqual(first_saved_item.list, list_user)
+        self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_user)
